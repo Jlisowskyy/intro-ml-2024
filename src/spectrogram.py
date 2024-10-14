69563b8aaa08fafbb22b3f15d2362629d8ab4e9c
pietraldo <pietrzakjakub4321@gmail.com>
2024-10-14 20:32:24 +0200

    some estetical changes

--- src/spectrogram.py
+++ src/spectrogram.py
@@ -1,29 +1,37 @@
-import numpy as np
-import soundfile as sf 
-import matplotlib.pyplot as plt # for ploting
 import os
+from io import BytesIO
+import numpy as np
+import matplotlib.pyplot as plt 
 import librosa
 import librosa.display
 from PIL import Image
-from io import BytesIO
+
 
 
 
 """
-Function generates mel-frequency spectrogram based on audio data. Audio data is a numpy array. It retrurns numpy array that represents
+Function generates mel-frequency spectrogram based on audio data. Audio data is a numpy array. It 
+retrurns numpy array that represents - 
 image of spectrogram.
-It uses Mel scale, which is more aligned with human hearing perception. This makes it effective for speech or voice-based tasks.
-Mel scale emphasizes the lower frequencies where most of the speech information resides while compressing the higher frequencies.
-This makes Mel spectrogram efficient for voice recognition and voice detection and emotion detection.
+It uses Mel scale, which is more aligned with human hearing perception. This makes it effective for
+speech or voice-based tasks.
+Mel scale emphasizes the lower frequencies where most of the speech information resides while 
+compressing the higher frequencies.
+This makes Mel spectrogram efficient for voice recognition and voice detection and emotion 
+detection.
 It underline sounds that human ear hears.
-Mel-frequency scale, which is a linear frequency space below 1000 Hz and a logarithmic space above 1000 Hz.
+Mel-frequency scale, which is a linear frequency space below 1000 Hz and a logarithmic space above
+1000 Hz.
 """
-def gen_spectrogram(audio_data, sample_rate, save_to_file=True, save_path="./image_spectrograms/spectrogram.png", show_axis=False, width=400, height=300):
+def gen_spectrogram(audio_data, sample_rate, save_to_file=True, 
+                    save_path="./image_spectrograms/spectrogram.png", 
+                    show_axis=False, width=400, height=300):
     dpi = 100
     fmax = 8000
 
     # Generate Mel spectrogram
-    S = librosa.feature.melspectrogram(y=audio_data, sr=sample_rate, n_fft=4096, hop_length=512, n_mels=512, fmax=fmax)
+    S = librosa.feature.melspectrogram(y=audio_data, sr=sample_rate, 
+                                       n_fft=4096, hop_length=512, n_mels=512, fmax=fmax)
     S_db = librosa.power_to_db(S, ref=np.max)
 
     # Set figure size in inches (for the given width and height in pixels)
@@ -33,12 +41,13 @@ def gen_spectrogram(audio_data, sample_rate, save_to_file=True, save_path="./ima
 
     # Display Mel spectrogram with or without axis
     if show_axis:
-        img = librosa.display.specshow(S_db, sr=sample_rate, fmax=fmax, x_axis='time', y_axis='mel', ax=ax)
+        img = librosa.display.specshow(S_db, sr=sample_rate, fmax=fmax, 
+                                       x_axis='time', y_axis='mel', ax=ax)
         plt.colorbar(img, format='%+2.0f dB')
         plt.title('Mel-Frequency Spectrogram')
     else:
         img = librosa.display.specshow(S_db, sr=sample_rate, fmax=fmax, ax=ax)
-        plt.axis('off')  
+        plt.axis('off')
         plt.tight_layout(pad=0)
         plt.subplots_adjust(left=0, right=1, top=1, bottom=0)
 
@@ -67,16 +76,3 @@ def save_figure(file_name):
     if not os.path.exists(output_directory):
         os.makedirs(output_directory)
     output_file_path = os.path.join(output_directory, file_name)
-    
-    
-
-
-# example
-"""
-audio_file_path="C:/Users/pietr/Documents/studia/machine learning/projekt/cutted_audios/class0/f2_script1_ipad_office1_5000.wav"
-audio_data, sample_rate = sf.read(audio_file_path)
-image_array=gen_spectrogram(audio_data, sample_rate)
-plt.imshow(image_array)
-plt.axis('off')
-plt.show()
-"""
