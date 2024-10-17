d03373919e611a4aaccd0c42f37130e290d15c0b
Tomasz Mycielski <60585367+mycielskit@users.noreply.github.com>
2024-10-17 15:50:16 +0200

    pylint -_-

--- src/helper_scripts/data_analysis.py
+++ src/helper_scripts/data_analysis.py
@@ -5,7 +5,7 @@ from os import walk
 from os.path import join
 from scipy.io import wavfile
 
-sr_anomaly = False
+SR_ANOMALY = False
 min_len = float('inf')
 max_len = 0
 
@@ -18,11 +18,11 @@ for root, dirs, files in walk('./datasets/daps'):
         duration = len(data) / sr
         print(f'{i}: {sr} Hz, {duration} s')
         if sr != 44100:
-            sr_anomaly = True
+            SR_ANOMALY = True
         min_len = min(min_len, duration)
         max_len = max(max_len, duration)
 
-if sr_anomaly:
+if SR_ANOMALY:
     print('Not every file is 44.1 kHz')
 else:
     print('Every file is 44.1 kHz')
