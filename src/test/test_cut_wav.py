83fef15399970d2af915ed72f397712aa503846a
Jakub Lisowski <Jlisowskyy@gmail.com>
2024-10-17 18:24:51 +0200

    Added tests

--- src/test/test_cut_wav.py
+++ src/test/test_cut_wav.py
@@ -4,9 +4,12 @@ Author: Jakub Lisowski, 2024
 File contains manual tests for cutting the wav file
 """
 
+from pathlib import Path
+
 from src.audio.wav import cut_file_to_plain_chunk_files, WavIteratorType
 
-
+TEST_DATA_PATH = str(Path.resolve(Path(f'{__file__}/../test_data/cut_test_data.wav')))
+TEST_OUT_DIR = str(Path.resolve(Path(f'{__file__}/../test_tmp')))
 
 def manual_cut_test() -> None:
     """
@@ -14,8 +17,8 @@ def manual_cut_test() -> None:
     """
 
     cut_file_to_plain_chunk_files(
-        "data/voice.wav",
-        "data/voice_cut",
+        TEST_DATA_PATH,
+        TEST_OUT_DIR,
         5,
         WavIteratorType.PLAIN)
 
