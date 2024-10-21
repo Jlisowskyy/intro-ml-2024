9776e30d5dfe1585f049af09d8aef75f9020a95a
kwiatkowskim <kwiatkowski.michal.2003@gmail.com>
2024-10-21 21:29:49 +0200

    path fix

--- src/test/test_classify.py
+++ src/test/test_classify.py
@@ -6,19 +6,21 @@ audio, converts it into an `AudioData` object, and uses the `classify_wrapper` f
 the class of the audio.
 """
 
+from pathlib import Path
 import soundfile as sf
 import numpy as np
 from src.pipelines.classify import classify_wrapper
 from src.audio.audio_data import AudioData
 
-AUDIO_FILE_PATH = "/home/michal/Downloads/f2_script1_ipad_office1_35000.wav"
+# pylint: disable=line-too-long
+TEST_FILE_PATH = str(Path.resolve(Path(f'{__file__}/../test_data/f2_script1_ipad_office1_35000.wav')))
 
 def example_test_run():
     """
     Example function to run the audio processing pipeline on sample data.
     It creates a training pipeline and fits it on a single audio sample.
     """
-    audio_data_wav, sample_rate = sf.read(AUDIO_FILE_PATH)
+    audio_data_wav, sample_rate = sf.read(TEST_FILE_PATH)
     audio_data = AudioData(np.array(audio_data_wav), sample_rate)
 
     prediction = classify_wrapper(audio_data)
--- src/test/test_transformation_pipeline.py
+++ src/test/test_transformation_pipeline.py
@@ -6,15 +6,14 @@ spectrograms. The pipeline uses `AudioCleaner`, `AudioNormalizer`, and `Spectrog
 to transform the audio and save spectrograms as images.
 """
 
-import os
-from glob import glob
+from pathlib import Path
 import soundfile as sf
 import numpy as np
 from sklearn.pipeline import Pipeline
 from src.pipelines.audio_cleaner import AudioCleaner
 from src.pipelines.audio_normalizer import AudioNormalizer
-from src.pipelines.spectrogram_generator import SpectrogramGenerator, \
-    gen_mel_spectrogram, save_spectrogram
+# pylint: disable=line-too-long
+from src.pipelines.spectrogram_generator import SpectrogramGenerator, gen_mel_spectrogram, save_spectrogram
 from src.audio.audio_data import AudioData
 
 speaker_to_class = {
@@ -26,9 +25,9 @@ speaker_to_class = {
     'm8': 1
 }
 
-AUDIO_DIRECTORY_PATH = ""
-SPECTROGRAM_CLEANED_PATH = ""
-SPECTROGRAM_PATH = ""
+TEST_FILE_PATH = str(Path.resolve(Path(f'{__file__}/../test_data/f2_script1_ipad_office1_35000.wav')))
+SPECTROGRAM_CLEANED_PATH = str(Path.resolve(Path(f'{__file__}/../test_tmp/cleaned_spectrogram.png')))
+SPECTROGRAM_PATH = str(Path.resolve(Path(f'{__file__}/../test_tmp/spectrogram.png')))
 
 def example_test_run():
     """
@@ -43,44 +42,14 @@ def example_test_run():
         ('SpectrogramGenerator', SpectrogramGenerator())
     ])
 
-    (x_train, _) = get_data(AUDIO_DIRECTORY_PATH)
+    audio_data_wav, sample_rate = sf.read(TEST_FILE_PATH)
+    audio_data = AudioData(np.array(audio_data_wav), sample_rate)
 
     # Transformed data
-    transformation_pipeline.fit([x_train[0]])
-    model_input = transformation_pipeline.transform([x_train[0]])
+    transformation_pipeline.fit([audio_data])
+    model_input = transformation_pipeline.transform([audio_data])
 
     save_spectrogram(model_input[0], SPECTROGRAM_CLEANED_PATH)
 
-    spectrogram_not_cleaned = gen_mel_spectrogram(x_train[0].audio_signal, x_train[0].sample_rate)
+    spectrogram_not_cleaned = gen_mel_spectrogram(audio_data.audio_signal, audio_data.sample_rate)
     save_spectrogram(spectrogram_not_cleaned, SPECTROGRAM_PATH)
-
-def get_data(audio_directory_path):
-    """
-    Retrieves audio data and their corresponding classes from a given directory.
-
-    Args:
-        audio_directory_path (str): The path to the directory containing .wav files.
-
-    Returns:
-        tuple: A tuple containing a list of AudioData instances and their corresponding labels.
-    """
-    wav_files = glob(os.path.join(audio_directory_path, "*.wav"))
-
-    x_train = []
-    y_train = []
-
-    if not wav_files:
-        print("No .wav files found in the directory.")
-    else:
-        print(f"Found {len(wav_files)} .wav files.")
-        for wav_file_path in wav_files:
-            audio_data_wav, sample_rate = sf.read(wav_file_path)
-            audio_data = AudioData(np.array(audio_data_wav), sample_rate)
-
-            speaker = wav_file_path.split('_')[0]
-            speaker_class = speaker_to_class.get(speaker.lower(), 0)
-
-            x_train.append(audio_data)
-            y_train.append(speaker_class)
-
-    return (x_train, y_train)
