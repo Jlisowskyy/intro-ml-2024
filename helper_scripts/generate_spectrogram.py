b6c235bdd89879c95ee278dac38eab2c578b220b
pietraldo <pietrzakjakub4321@gmail.com>
2024-10-22 23:15:49 +0200

    script for generating and saving spectrogram

--- helper_scripts/generate_spectrogram.py
+++ helper_scripts/generate_spectrogram.py
@@ -1,19 +1,21 @@
 """
 Author: Jakub Pietrzak, 2024
+
+Modul for generating spectrograms and showing/saving it
 """
 
 import argparse
 import soundfile as sf
-import matplotlib.pyplot as plt  # Fix matplotlib import
+import matplotlib.pyplot as plt
 from sklearn.pipeline import Pipeline
-from src.spectrogram.spectrogram import gen_spectrogram, save_spectrogram  # Import both functions in one line
+from src.spectrogram.spectrogram import gen_spectrogram, save_spectrogram
 from src.audio.audio_data import AudioData
 from src.pipelines.audio_cleaner import AudioCleaner
-from src.pipelines.audio_normalizer import AudioNormalizer
 
 def main(sound_path: str, output_path: str = None, show: bool = False, clean_data: bool = False):
     """
-    Main function that processes the audio file, generates a spectrogram, and optionally cleans the data.
+    Main function that processes the audio file, generates a spectrogram, and optionally 
+    cleans the data.
 
     Args:
         sound_path (str): Path to the audio file.
@@ -21,27 +23,26 @@ def main(sound_path: str, output_path: str = None, show: bool = False, clean_dat
         show (bool): Flag to show the spectrogram using matplotlib.
         clean_data (bool): Flag to clean and normalize the audio data.
     """
-    
+
     data, samplerate = sf.read(sound_path)
-    audio_data = AudioData(data, samplerate)  
-    
-    
+    audio_data = AudioData(data, samplerate)
+
+
     if clean_data:
         transformation_pipeline = Pipeline(steps=[
-            ('AudioCleaner', AudioCleaner()),
-            ('AudioNormalizer', AudioNormalizer())
+            ('AudioCleaner', AudioCleaner())
         ])
         transformation_pipeline.fit([audio_data])
-        audio_data = transformation_pipeline.fit_transform([audio_data])[0]  
-    
-    
+        audio_data = transformation_pipeline.transform([audio_data])[0]
+
+
     spectrogram = gen_spectrogram(audio_data.audio_signal, audio_data.sample_rate)
-    
-    
+
+
     if output_path:
-        save_spectrogram(spectrogram, output_path)  
-    
-    
+        save_spectrogram(spectrogram, output_path)
+
+
     if show:
         plt.imshow(spectrogram)
         plt.axis('off')
@@ -49,13 +50,16 @@ def main(sound_path: str, output_path: str = None, show: bool = False, clean_dat
         plt.show()
 
 if __name__ == "__main__":
-    
+
     parser = argparse.ArgumentParser(description="Generates a spectrogram from an audio file.")
     parser.add_argument("sound_path", type=str, help="Path to the audio file.")
-    parser.add_argument('--output', '-o', type=str, help='Optional output file path for the spectrogram.')
-    parser.add_argument('--show', '-s', action='store_true', help='Show the spectrogram after generation.')
-    parser.add_argument('--clean', '-c', action='store_true', help='Clean and normalize the audio data.')
+    parser.add_argument('--output', '-o', type=str,
+                        help='Optional output file path for the spectrogram.')
+    parser.add_argument('--show', '-s', action='store_true',
+                        help='Show the spectrogram after generation.')
+    parser.add_argument('--clean', '-c', action='store_true',
+                        help='Clean and normalize the audio data.')
 
     args = parser.parse_args()
-    
+
     main(args.sound_path, args.output, args.show, args.clean)
