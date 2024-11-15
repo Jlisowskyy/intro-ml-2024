85bef7c8f4fb8ab1941d9a311cb808aeeb5e1d19
Jakub Pietrzak <150395814+pietraldo@users.noreply.github.com>
2024-11-15 02:18:01 +0100

    Kuba milestone2 (#96)
    - generating spectrograms from random dataset
    - changed generating histogram script
    - add docstring schema file

--- src/helper_scripts/from_wav_to_histogram.py
+++ src/helper_scripts/from_wav_to_histogram.py
@@ -8,52 +8,17 @@ import os
 import sys
 from pathlib import Path
 
-import numpy as np
 import soundfile as sf
-from PIL import Image
-from matplotlib import pyplot as plt
 from sklearn.pipeline import Pipeline
 
+from src.helper_scripts.generate_rgb_histogram import generate_rgb_histogram
 from src.audio.audio_data import AudioData
 from src.audio.spectrogram import gen_mel_spectrogram, save_spectrogram
 from src.pipelines.audio_cleaner import AudioCleaner
+from src.pipelines.audio_normalizer import AudioNormalizer
 
 
-def cut_audio_to_5_seconds(directory):
-    """
-    Function that cuts audio files to 5 seconds parts
-    Args:
-        directory (str): Path to the directory with audio files.
-    """
-    if not os.path.exists(directory):
-        print("directory does not exist")
-        return ""
-
-    output_directory = os.path.join(directory, "_5_seconds_audio")
-    if not os.path.exists(output_directory):
-        os.makedirs(output_directory)
-
-    files = os.listdir(directory)
-    for index,file in enumerate(files):
-        if file.endswith(".wav"):
-            data, samplerate = sf.read(os.path.join(directory, file))
-
-            #cut into 5 seconds parts
-            length=len(data)
-            for i in range(0,length,samplerate*5):
-                name = os.path.join(output_directory, file[:-4] + "_" + str(i) + ".wav")
-                data_part=data[i:i+samplerate*5]
-                if len(data_part)<samplerate*5:
-                    break
-                sf.write(name,  data_part, samplerate)
-
-        print(f"Cutting: Done with {file}, {index}")
-
-    print("Done with cutting audio files to 5 seconds parts")
-    return output_directory
-
-
-def create_spectrograms(directory, denoise=False):
+def create_spectrogram(directory, denoise=False):
     """
     Function that creates spectrograms from audio files
     Args:
@@ -70,134 +35,16 @@ def create_spectrograms(directory, denoise=False):
             audio_data = AudioData(data, samplerate)
             if denoise:
                 transformation_pipeline = Pipeline(steps=[
+                    ('AudioNormalizer', AudioNormalizer()),
                     ('AudioCleaner', AudioCleaner())
                 ])
-                transformation_pipeline.fit([data])
+                transformation_pipeline.fit([audio_data])
                 audio_data = transformation_pipeline.transform([audio_data])[0]
             spectrogram = gen_mel_spectrogram(audio_data.audio_signal, samplerate)
             save_spectrogram(spectrogram, os.path.join(output_directory, file[:-4] + ".png"))
             print(f"Spectrogram: Done with {file}, {index}")
     print("Done with creating spectrograms")
     return output_directory
-
-
-def create_rgb_histogram(directory):  # pylint: disable=too-many-locals
-    """
-    Function that creates rgb histograms from spectrograms
-    Args:
-        directory (str): Path to the directory with spectrograms.
-    """
-    files = os.listdir(directory)
-    class1 = ["f1", "f7", "f8", "m3", "m6", "m8"]
-    array_class1 = [[0] * 256 for _ in range(3)]
-    array_class0 = [[0] * 256 for _ in range(3)]
-
-    num_class1 = 0
-    num_class0 = 0
-
-    for index, file_name in enumerate(files):
-        image = Image.open(os.path.join(directory, file_name))
-        image_array = np.array(image)
-
-        # Separate the color channels
-        r_channel = image_array[:, :, 0].flatten()
-        g_channel = image_array[:, :, 1].flatten()
-        b_channel = image_array[:, :, 2].flatten()
-
-        counts_r = np.bincount(r_channel, minlength=256)
-        counts_g = np.bincount(g_channel, minlength=256)
-        counts_b = np.bincount(b_channel, minlength=256)
-
-        if file_name.split("_")[0] in class1:
-            num_class1 += 1
-            for i in range(256):
-                array_class1[0][i] += counts_r[i]
-                array_class1[1][i] += counts_g[i]
-                array_class1[2][i] += counts_b[i]
-        else:
-            num_class0 += 1
-            for i in range(256):
-                array_class0[0][i] += counts_r[i]
-                array_class0[1][i] += counts_g[i]
-                array_class0[2][i] += counts_b[i]
-        print(f"Histogram: Done with {file_name}, {index}")
-    print("Done with creating rgb histograms")
-
-    for i in range(0, 255):
-        for j in range(3):
-            if num_class1!=0:
-                array_class1[j][i] /= num_class1
-            if num_class0!=0:
-                array_class0[j][i] /= num_class0
-
-    return array_class1, array_class0
-
-def show_rgb_histogram(array_class1, array_class0):
-    """
-    shows rgb histogram
-
-    Args:
-        array_class1 (_type_): array [3][256]
-        array_class0 (_type_): array [3][256]
-    """
-    intensity_levels = np.arange(256)
-
-    plt.figure(figsize=(12, 6))
-    plt.subplot(1, 2, 1)
-    plt.bar(intensity_levels, array_class0[0], color='red', alpha=0.5, label='Red')
-    plt.bar(intensity_levels, array_class0[1], color='green', alpha=0.5, label='Green')
-    plt.bar(intensity_levels, array_class0[2], color='blue', alpha=0.5, label='Blue')
-    plt.title('RGB Histogram - Class 0')
-    plt.xlabel('Intensity Level')
-    plt.ylabel('Frequency')
-    plt.xlim(0, 255)
-    plt.legend()
-    plt.grid(axis='y')
-
-    plt.subplot(1, 2, 2)
-    plt.bar(intensity_levels, array_class1[0], color='red', alpha=0.5, label='Red')
-    plt.bar(intensity_levels, array_class1[1], color='green', alpha=0.5, label='Green')
-    plt.bar(intensity_levels, array_class1[2], color='blue', alpha=0.5, label='Blue')
-    plt.title('RGB Histogram - Class 1')
-    plt.xlabel('Intensity Level')
-    plt.ylabel('Frequency')
-    plt.xlim(0, 255)
-    plt.legend()
-    plt.grid(axis='y')
-
-    plt.tight_layout()
-    plt.show()
-
-def delete_silence(data, threshold):
-    """
-    Function that deletes silence from audio data
-    Args:
-        data (np.array): Audio data.
-        threshold (float): Threshold for silence.
-    """
-    return data[np.where(np.abs(data) > threshold)]
-
-
-def delete_silence_in_files(directory, threshold):
-    """
-    Function that deletes silence from audio data
-    Args:
-        data (np.array): Audio data.
-        threshold (float): Threshold for silence.
-    """
-    output_directory = os.path.join(directory, "_silence_deleted")
-    if not os.path.exists(output_directory):
-        os.makedirs(output_directory)
-    files = os.listdir(directory)
-    for index,file in enumerate(files):
-        if file.endswith(".wav"):
-            data, samplerate = sf.read(os.path.join(directory, file))
-            data = delete_silence(data, threshold)
-            sf.write(os.path.join(output_directory, file), data, samplerate)
-            print(f"Silence: Done with {file}, {index}")
-    print("Done with deleting silence")
-    return output_directory
-
 
 def process_directory(directory: str) -> None:
     """
@@ -206,12 +53,10 @@ def process_directory(directory: str) -> None:
 
     :param directory: Path to the directory with audio files
     """
-
-    output_directory = delete_silence_in_files(directory, 0.01)
-    output_directory = cut_audio_to_5_seconds(output_directory)
-    output_directory = create_spectrograms(output_directory, True)
-    array_class1, array_class0 = create_rgb_histogram(output_directory)
-    show_rgb_histogram(array_class1, array_class0)
+    spectrogram_dir = create_spectrogram(directory, False)
+    for file in os.listdir(spectrogram_dir):
+        spectrogram_path = os.path.join(spectrogram_dir, file)
+        generate_rgb_histogram(spectrogram_path)
 
 
 DEFAULT_DIR = str(Path.resolve(Path(f'{__file__}/../work_dir')))
--- src/helper_scripts/spectrogram_script.py
+++ src/helper_scripts/spectrogram_script.py
@@ -4,6 +4,8 @@ Author: Jakub Pietrzak, 2024
 Modul for generating spectrograms and showing/saving it
 """
 
+import os
+import random
 import argparse
 
 import matplotlib.pyplot as plt
@@ -14,45 +16,76 @@ from src.audio.audio_data import AudioData
 from src.audio.spectrogram import gen_mel_spectrogram, gen_spectrogram, save_spectrogram
 from src.pipelines.audio_cleaner import AudioCleaner
 
+def get_random_audio_path(dir_path: str) -> str:
+    """
+    Gets a random audio file path from a directory.
+    Args:
+        dir_path (str): path to direstory with audio files
 
-def process(sound_path: str, output_path: str = None, show: bool = False,
+    Returns:
+        str: file path to a random audio file.
+    """
+    while True:
+        files = [f for f in os.listdir(dir_path) if f.endswith(".wav")]
+        if files:
+            return os.path.join(dir_path, random.choice(files))
+        subdirectories = [d for d in os.listdir(dir_path)
+                          if os.path.isdir(os.path.join(dir_path, d))]
+        if not subdirectories:
+            raise FileNotFoundError("No `.wav` files found in the directory or its subdirectories.")
+        dir_path = os.path.join(dir_path, random.choice(subdirectories))
+
+def process(sound_path: str = "", directory: str = "", number_of_samples: int = 1, output_path:
+            str = None, show: bool = False,
             mel: bool = False, clean_data: bool = False, show_axis: bool = False):
+    # pylint: disable=too-many-locals
     """
     Process function that processes the audio file, generates a spectrogram, and optionally
     cleans the data.
 
     Args:
         sound_path (str): Path to the audio file.
+        directory (str): Path to the directory with audio files to generate randomly.
+        number_of_samples (int): Number of samples to generate.
         output_path (str): Optional output path for the spectrogram image.
         show (bool): Flag to show the spectrogram using matplotlib.
         mel (bool): Flag to generate a mel-frequency spectrogram.
         clean_data (bool): Flag to clean and normalize the audio data.
         show_axis (bool): Flag to show axis on the spectrogram plot.
     """
-    data, samplerate = sf.read(sound_path)
-    audio_data = AudioData(data, samplerate)
 
-    if clean_data:
-        transformation_pipeline = Pipeline(steps=[
-            ('AudioCleaner', AudioCleaner())
-        ])
-        transformation_pipeline.fit([audio_data])
-        audio_data = transformation_pipeline.transform([audio_data])[0]
+    for i in range(number_of_samples):
+        new_sound_path = sound_path
+        if sound_path == "" or sound_path is None:
+            new_sound_path = get_random_audio_path(directory)
+        print(new_sound_path)
 
-    if mel:
-        spectrogram = gen_mel_spectrogram(audio_data.audio_signal,
+        data, samplerate = sf.read(new_sound_path)
+        audio_data = AudioData(data, samplerate)
+
+        if clean_data:
+            transformation_pipeline = Pipeline(steps=[
+                ('AudioCleaner', AudioCleaner())
+            ])
+            transformation_pipeline.fit([audio_data])
+            audio_data = transformation_pipeline.transform([audio_data])[0]
+
+        if mel:
+            spectrogram = gen_mel_spectrogram(audio_data.audio_signal,
+                                            audio_data.sample_rate, show_axis)
+        else:
+            spectrogram = gen_spectrogram(audio_data.audio_signal,
                                           audio_data.sample_rate, show_axis)
-    else:
-        spectrogram = gen_spectrogram(audio_data.audio_signal, audio_data.sample_rate, show_axis)
 
-    if output_path:
-        save_spectrogram(spectrogram, output_path)
+        if output_path:
+            splited_path=output_path.split(".")
+            save_spectrogram(spectrogram, splited_path[0]+str(i)+"."+splited_path[1])
 
-    if show:
-        plt.imshow(spectrogram)
-        plt.axis('off')
-        plt.subplots_adjust(left=0, right=1, top=1, bottom=0)
-        plt.show()
+        if show:
+            plt.imshow(spectrogram)
+            plt.axis('off')
+            plt.subplots_adjust(left=0, right=1, top=1, bottom=0)
+            plt.show()
 
 
 def main(argv: list[str]) -> None:
@@ -63,7 +96,11 @@ def main(argv: list[str]) -> None:
         argv (List[str]): List of command line arguments.
     """
     parser = argparse.ArgumentParser(description="Generates a spectrogram from an audio file.")
-    parser.add_argument("sound_path", type=str, help="Path to the audio file.")
+    parser.add_argument("--file_path", '-f', type=str, help="Path to the audio file.")
+    parser.add_argument('--number', '-n', type=int,
+                        help='number of samples')
+    parser.add_argument('--directory', '-d', type=str,
+                        help='Path to the directory with audio files.')
     parser.add_argument('--output', '-o', type=str,
                         help='Optional output file path for the spectrogram.')
     parser.add_argument('--show', '-s', action='store_true',
@@ -76,7 +113,8 @@ def main(argv: list[str]) -> None:
                         help='Show axis on the spectrogram plot.')
 
     args = parser.parse_args(argv)
-    process(args.sound_path, args.output, args.show, args.mel, args.clean, args.show_axis)
+    process(args.file_path, args.directory, args.number, args.output, args.show, args.mel,
+            args.clean, args.show_axis)
 
 
 if __name__ == "__main__":
