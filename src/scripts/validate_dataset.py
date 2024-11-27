5532c51240f62a44f1dce6b88643223d1ce11167
Jakub Lisowski <Jlisowskyy@gmail.com>
2024-11-27 01:38:23 +0100

    Temp fix

--- src/scripts/validate_dataset.py
+++ src/scripts/validate_dataset.py
@@ -7,12 +7,12 @@ import torch
 import torch.utils
 from torch.utils.data import DataLoader
 
-from src.cnn.cnn import BaseCNN
 from src.cnn.loadset import MultiLabelDataset
 from src.cnn.train import test
 from src.constants import (TRAINING_TEST_SET_SIZE, TRAINING_VALIDATION_SET_SIZE,
                            TRAINING_TRAIN_SET_SIZE, DATABASE_ANNOTATIONS_PATH,
                            DATABASE_OUT_PATH, MODEL_BASE_PATH)
+from src.model_definitions import BasicCNN
 
 
 def main() -> None:
@@ -22,11 +22,8 @@ def main() -> None:
 
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-    cnn = BaseCNN()  # TODO: fix this
-    cnn.load_state_dict(torch.load(MODEL_BASE_PATH,
-                                   map_location=torch.device(device),
-                                   weights_only=True))
-    cnn.to(device)
+    cnn = BasicCNN()  # TODO: REPLACE IT WITH YOUR MODEL
+    cnn.load_model(MODEL_BASE_PATH)
 
     dataset = MultiLabelDataset(
         DATABASE_ANNOTATIONS_PATH,
