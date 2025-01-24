11fd244909227aaa2c2e1ecf04a8600a65fbb73c
Jakub Pietrzak <150395814+pietraldo@users.noreply.github.com>
2025-01-24 21:06:54 +0100

    split unknown into 4 classes (#140)
    Changed unknown class to 4 new unknown classes: unknown1, unknown2,
    unknown3, unknown4.
    Because the unknown class was too big and to much data was label as one
    class
    
    ---------
    
    Co-authored-by: Jakub Lisowski <Jlisowskyy@gmail.com>
    Co-authored-by: Jakub Lisowski <115403674+Jlisowskyy@users.noreply.github.com>

--- src/scripts/regenerate_csv.py
+++ src/scripts/regenerate_csv.py
@@ -7,14 +7,14 @@ Simply run this script on whichever processed dataset you want to generate a rel
 
 from os import walk
 
-from src.constants import DATABASE_ANNOTATIONS_PATH, DATABASE_OUT_PATH, CLASSES
+from src.constants import DATABASE_ANNOTATIONS_PATH, DATABASE_OUT_PATH, CLASSES, NUM_CLASSES_UNKNOWN
 
 
 def main() -> None:
     """
     Script entry point
     """
-
+    files_unknown = 0
     print('Regenerating annotations.csv')
     with open(DATABASE_ANNOTATIONS_PATH, 'w', encoding='UTF-8') as f:
         f.write('folder,file_name,classID\n')
@@ -22,5 +22,10 @@ def main() -> None:
             class_id = root.split('/')[-1]
             if class_id not in CLASSES:
                 class_id = 'unknown'
+
             for i in files:
-                f.write(f'{root},{i},{class_id}\n')
+                label = class_id
+                if class_id == 'unknown':
+                    files_unknown += 1
+                    label = 'unknown'+str(files_unknown % NUM_CLASSES_UNKNOWN+1)
+                f.write(f'{root},{i},{label}\n')
