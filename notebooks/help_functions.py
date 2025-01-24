22528d0bdef55538010dbdc9796a52ab702dfa3b
pietraldo <pietrzakjakub4321@gmail.com>
2025-01-25 00:51:57 +0100

    pylint

--- notebooks/help_functions.py
+++ notebooks/help_functions.py
@@ -1,6 +1,9 @@
 from IPython.display import Audio, display
 from src.pipeline.audio_data import AudioData
 
+"""
+    Function to display an audio in jupyter notebook.
+"""
 def display_audio(audio_data: AudioData) -> None:
     """
     Displays an audio signal as an audio player widget.
