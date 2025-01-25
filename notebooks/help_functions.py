c912df8ae82aebeb95aa2d05e10bc1852ab5701d
Jakub Pietrzak <150395814+pietraldo@users.noreply.github.com>
2025-01-25 21:17:29 +0100

    Notebooks (#148)
    created and corrected notebooks

--- notebooks/help_functions.py
+++ notebooks/help_functions.py
@@ -1,3 +1,7 @@
+"""
+    Functions that help in notebook.
+"""
+
 from IPython.display import Audio, display
 from src.pipeline.audio_data import AudioData
 
@@ -13,3 +17,4 @@ def display_audio(audio_data: AudioData) -> None:
         audio_data (AudioData): The audio data to display.
     """
     display(Audio(audio_data.audio_signal, rate=audio_data.sample_rate))
+    
