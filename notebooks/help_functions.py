9351f378522935159509edca0101917e30c215e7
Jakub Pietrzak <150395814+pietraldo@users.noreply.github.com>
2025-01-25 21:15:43 +0100

    Notebook kuba (#147)
    created and corrected notebooks

--- notebooks/help_functions.py
+++ notebooks/help_functions.py
@@ -1,10 +1,10 @@
+"""
+    Functions that help in notebook.
+"""
+
 from IPython.display import Audio, display
 from src.pipeline.audio_data import AudioData
 
-
-"""
-    Function to display an audio in jupyter notebook.
-"""
 def display_audio(audio_data: AudioData) -> None:
     """
     Displays an audio signal as an audio player widget.
@@ -13,3 +13,4 @@ def display_audio(audio_data: AudioData) -> None:
         audio_data (AudioData): The audio data to display.
     """
     display(Audio(audio_data.audio_signal, rate=audio_data.sample_rate))
+    
